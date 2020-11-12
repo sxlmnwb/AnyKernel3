8e93613122e234cf5e33fef5f15236fe7b149747
Risti699 <61613645+Risti699@users.noreply.github.com>
2020-11-12 12:04:43 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=JFLA Kernel by Kaguyaasama @ Telegram
+kernel.string=Ōtsutsuki Kernel by Kaguyaasama @ Telegram
 do.devicecheck=1
 do.modules=1
 do.cleanup=1
