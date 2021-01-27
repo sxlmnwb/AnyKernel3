610c7bced97cac48965de748ec41e91be4721e46
Risti699 <61613645+Risti699@users.noreply.github.com>
2021-01-27 09:21:30 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=JFLA-Karamel Kernel by Kaguyaasama @ Telegram
+kernel.string=JandaX Kernel by Kaguyaasama @ Telegram
 do.devicecheck=1
 do.modules=1
 do.cleanup=1
