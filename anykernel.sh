5ee8ccfef85f2e4ea5fd74eb500ffbcc12293d2a
Salman Wahib <65052912+sxlmnwb@users.noreply.github.com>
2021-02-06 03:38:31 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=JandaX Kernel by Kaguyaasama @ Telegram
+kernel.string=NgockopTempiX Kernel by sxlmnwb @ Telegram
 do.devicecheck=1
 do.modules=1
 do.cleanup=1
