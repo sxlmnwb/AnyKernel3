9036e5f7c8584acc30e44729c954675929959b20
Salman Wahib <65052912+sxlmnwb@users.noreply.github.com>
2021-02-10 04:10:49 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ShirakamiFubuki Kernel by sxlmnwb @ Telegram
+kernel.string=sxlzpt//LLVM Kernel by sxlmnwb @ Telegram
 do.devicecheck=1
 do.modules=1
 do.cleanup=1
