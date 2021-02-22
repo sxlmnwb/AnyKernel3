aafba86e43b4800572e6fe96f3d9fa4262fc05fb
Salman Wahib <65052912+sxlmnwb@users.noreply.github.com>
2021-02-22 22:22:41 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=sxlzptLLVM Kernel by sxlmnwb @ Telegram
+kernel.string=sxlzptZEUS Kernel by sxlmnwb @ Telegram
 do.devicecheck=1
 do.modules=1
 do.cleanup=1
